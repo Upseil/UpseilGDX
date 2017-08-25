b52c31957cedacf525b3697e10a10c68e7524660
Lennart Hensler <LennartHensler@aol.com>
2017-08-25 19:53:20 +0200

    Added RawConfig and an AbstractApplicationAdapter

---
+++ upseilgdx/core/src/com/upseil/gdx/AbstractApplicationAdapter.java
@@ -0,0 +1,35 @@
+package com.upseil.gdx;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Colors;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.utils.ObjectMap.Entry;
+
+public abstract class AbstractApplicationAdapter extends ApplicationAdapter {
+    
+    @Override
+    public void render() {
+        render(Gdx.graphics.getDeltaTime());
+    }
+
+    protected abstract void render(float deltaTime);
+
+    protected Skin loadSkin(String path, boolean registerColors, boolean enableFontMarkup) {
+        Skin skin = new Skin(Gdx.files.internal(path));
+        if (registerColors) {
+            for (Entry<String, Color> colorEntry : skin.getAll(Color.class).entries()) {
+                Colors.put(colorEntry.key, colorEntry.value);
+            } 
+        }
+        if (enableFontMarkup) {
+            for (Entry<String, BitmapFont> fontEntry : skin.getAll(BitmapFont.class)) {
+                fontEntry.value.getData().markupEnabled = true;
+            }
+        }
+        return skin;
+    }
+    
+}
---
+++ upseilgdx/core/src/com/upseil/gdx/config/RawConfig.java
@@ -0,0 +1,150 @@
+package com.upseil.gdx.config;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.JsonReader;
+import com.badlogic.gdx.utils.JsonValue;
+import com.upseil.gdx.util.DoubleBasedExpression;
+
+/**
+ * Allows loading a json-based config into memory and access its values. No key
+ * in the json document should contain a dot as it's used for accessing nested
+ * values.
+ * 
+ * Example Json Document:
+ * 
+ * { 
+ *     "room": { 
+ *         "floorBase": 1.01, 
+ *         "roomBase": 1.025, 
+ *         "minSize": 7, 
+ *         "maxSize": 100, 
+ *         "sizeVariance": 1.4 
+ *     } 
+ * }
+ * 
+ * The values can now be accessed like this:
+ * 
+ * int minRoomSize = config.getInt("room.minSize");
+ * double floorBase = config.getDouble("room.floorBase");
+ * 
+ * @author frederik
+ *        
+ */
+public class RawConfig {
+    
+    public static JsonValue loadJson(String path) {
+        FileHandle handle = Gdx.files.internal(path);
+        JsonValue jsonRoot;
+        try (InputStream jsonStream = handle.read()) {
+            JsonReader reader = new JsonReader();
+            jsonRoot = reader.parse(jsonStream);
+        } catch (IOException e) {
+            throw new RuntimeException("Could not load config file with path: " + path, e);
+        }
+        return jsonRoot;
+    }
+    
+    public static RawConfig loadConfig(String path) {
+        return new RawConfig(null, loadJson(path));
+    }
+    
+    private final RawConfig parent;
+    private final JsonValue root;
+    
+    public RawConfig(RawConfig parent, JsonValue jsonRoot) {
+        this.parent = parent;
+        this.root = jsonRoot;
+    }
+    
+    public RawConfig getParent() {
+        return parent;
+    }
+    
+    public float getFloat(String key) {
+        JsonValue node = getNode(key);
+        return node.asFloat();
+    }
+    
+    public double getDouble(String key) {
+        JsonValue node = getNode(key);
+        return node.asDouble();
+    }
+    
+    public int getInt(String key) {
+        JsonValue node = getNode(key);
+        return node.asInt();
+    }
+
+    public boolean getBoolean(String key) {
+        return getNode(key).asBoolean();
+    }
+    
+    public DoubleBasedExpression getExpression(String key) {
+        return new DoubleBasedExpression(getString(key));
+    }
+    
+    public <T extends Enum<T>> T getEnum(String key, Class<T> type) {
+        return Enum.valueOf(type, getString(key));
+    }
+    
+    public String getString(String key) {
+        JsonValue node = getNode(key);
+        return node.asString();
+    }
+    
+    public boolean hasNode(String key) {
+        return getNode(key) != null;
+    }
+    
+    public RawConfig getChild(String key) {
+        JsonValue node = getNode(key);
+        if (node == null) {
+            return null;
+        }
+        if (node.isValue()) {
+            throw new IllegalArgumentException("The node found isn't an array or object."); 
+        }
+        return new RawConfig(this, node);
+    }
+    
+    private JsonValue getNode(String key) {
+        String[] split = key.split("\\.");
+        JsonValue currentNode = root;
+        for (String keyPart : split) {
+            currentNode = currentNode.get(keyPart);
+            if (currentNode == null) {
+                break;
+            }
+        }
+        return currentNode;
+    }
+    
+    public Iterable<RawConfig> getChildren() {
+        if (!isArray()) {
+            throw new IllegalStateException("This config is no array");
+        }
+        Array<RawConfig> children = new Array<>(size());
+        for (JsonValue node : root) {
+            children.add(new RawConfig(this, node));
+        }
+        return children;
+    }
+    
+    public boolean isNull() {
+        return root == null || root.isNull();
+    }
+    
+    public boolean isArray() {
+        return root.isArray();
+    }
+    
+    public int size() {
+        return isArray() ? 0 : root.size;
+    }
+    
+}
