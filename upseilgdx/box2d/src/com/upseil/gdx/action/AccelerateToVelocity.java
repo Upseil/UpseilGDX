23bd8d99e6c1b6552baae8bd3acff7c28f5b9d6c
Lennart Hensler <LennartHensler@aol.com>
2017-12-13 19:16:58 +0100

    Fixed the box2d actions

--- upseilgdx/box2d/src/com/upseil/gdx/action/AccelerateToVelocity.java
+++ upseilgdx/box2d/src/com/upseil/gdx/action/AccelerateToVelocity.java
@@ -6,43 +6,60 @@ import com.badlogic.gdx.physics.box2d.Body;
 
 public class AccelerateToVelocity extends AbstractAction<Body, AccelerateToVelocity> {
 
-    private float acceleration;
+    private Vector2 acceleration;
     private Vector2 targetVelocity;
-    private Vector2 desiredVelocity;
+    private boolean holdVelocity;
     
     public AccelerateToVelocity() {
+        acceleration = new Vector2();
         targetVelocity = new Vector2();
-        desiredVelocity = new Vector2();
     }
 
     @Override
     public boolean act(float deltaTime) {
         Vector2 currentVelocity = getState().getLinearVelocity();
-        if (targetVelocity.epsilonEquals(currentVelocity, MathUtils.FLOAT_ROUNDING_ERROR)) return true;
-        
+        if (targetVelocity.epsilonEquals(currentVelocity, MathUtils.FLOAT_ROUNDING_ERROR)) return !holdVelocity;
+
         float xDirection = Math.signum(targetVelocity.x - currentVelocity.x);
+        float desiredXVelocity = currentVelocity.x + xDirection * acceleration.x * deltaTime;
+        if ((xDirection < 0 && desiredXVelocity < targetVelocity.x) || (xDirection > 0 && desiredXVelocity > targetVelocity.x)) {
+            desiredXVelocity = targetVelocity.x;
+        }
+        
         float yDirection = Math.signum(targetVelocity.y - currentVelocity.y);
-//        desiredVelocity.x = MathUtils.clamp(currentVelocity.x + xDirection * acceleration * deltaTime, -targetVelocity.x, targetVelocity.x);
-//        desiredVelocity.y = MathUtils.clamp(currentVelocity.y + yDirection * acceleration * deltaTime, -targetVelocity.y, targetVelocity.y);
-        desiredVelocity.x = Math.min(Math.abs(currentVelocity.x) + acceleration * deltaTime, Math.abs(targetVelocity.x)) * xDirection;
-        desiredVelocity.y = Math.min(Math.abs(currentVelocity.y) + acceleration * deltaTime, Math.abs(targetVelocity.y)) * yDirection; 
+        float desiredYVelocity = currentVelocity.y + yDirection * acceleration.y * deltaTime;
+        if ((yDirection < 0 && desiredYVelocity < targetVelocity.y) || (yDirection > 0 && desiredYVelocity > targetVelocity.y)) {
+            desiredYVelocity = targetVelocity.y;
+        }
         
         float mass = getState().getMass();
-        float impulseX = mass * (desiredVelocity.x - currentVelocity.x);
-        float impulseY = mass * (desiredVelocity.y - currentVelocity.y);
+        float impulseX = mass * (desiredXVelocity - currentVelocity.x);
+        float impulseY = mass * (desiredYVelocity - currentVelocity.y);
         
         Vector2 position = getState().getPosition();
         getState().applyLinearImpulse(impulseX, impulseY, position.x, position.y, true);
         
         return false;
     }
-
-    public float getAcceleration() {
+    
+    public Vector2 getAcceleration() {
         return acceleration;
     }
+    
+    public void setAcceleration(Vector2 acceleration) {
+        setAcceleration(acceleration.x, acceleration.y);
+    }
 
-    public void setAcceleration(float acceleration) {
-        this.acceleration = acceleration;
+    public void setAcceleration(float xAcceleration, float yAcceleration) {
+        acceleration.set(xAcceleration, yAcceleration);
+    }
+    
+    public void setAccelerationX(int xAcceleration) {
+        acceleration.x = xAcceleration;
+    }
+    
+    public void setAccelerationY(int yAcceleration) {
+        acceleration.y = yAcceleration;
     }
     
     public Vector2 getTargetVelocity() {
@@ -65,12 +82,20 @@ public class AccelerateToVelocity extends AbstractAction<Body, AccelerateToVeloc
         targetVelocity.y = yVelocity;
     }
 
+    public boolean holdVelocity() {
+        return holdVelocity;
+    }
+
+    public void setHoldVelocity(boolean holdVelocity) {
+        this.holdVelocity = holdVelocity;
+    }
+
     @Override
     public void reset() {
         super.reset();
-        acceleration = Float.MAX_VALUE;
+        acceleration.set(0, 0);
         targetVelocity.set(0, 0);
-        desiredVelocity.set(0, 0);
+        holdVelocity = false;
     }
     
 }
--- upseilgdx/box2d/src/com/upseil/gdx/action/RotateToAngle.java
+++ upseilgdx/box2d/src/com/upseil/gdx/action/RotateToAngle.java
@@ -14,12 +14,14 @@ public class RotateToAngle extends AbstractAction<Body, RotateToAngle> {
         if (MathUtils.isEqual(targetAngle, currentAngle)) return true;
         
         float currentVelocity = getState().getAngularVelocity();
-        float nextAngle = currentAngle + currentVelocity * deltaTime;
+        float nextAngle = currentAngle + currentVelocity * (deltaTime * 50);
         float angleToRotate = targetAngle - nextAngle;
         while (angleToRotate < -MathUtils.PI) angleToRotate += MathUtils.PI2;
         while (angleToRotate >  MathUtils.PI) angleToRotate -= MathUtils.PI2;
         
-        float desiredVelocity = MathUtils.clamp(angleToRotate / deltaTime, -velocityLimit, velocityLimit);
+        float desiredVelocity = angleToRotate / (deltaTime * 50);
+        desiredVelocity = Math.min(velocityLimit, Math.max(desiredVelocity, -velocityLimit));
+        
         float impulse = getState().getInertia() * desiredVelocity;
         getState().applyAngularImpulse(impulse, true);
         return false;
