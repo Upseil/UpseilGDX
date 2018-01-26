5dd4cd10e3dc27fca550b8aaf1933671dc36e3a9
Lennart Hensler <lennarthensler@aol.com>
2018-01-26 16:54:59 +0100

    The clear screen color of the ClearScreenSystem can now be specified

--- upseilgdx/artemis/src/com/upseil/gdx/artemis/system/ClearScreenSystem.java
+++ upseilgdx/artemis/src/com/upseil/gdx/artemis/system/ClearScreenSystem.java
@@ -2,14 +2,33 @@ package com.upseil.gdx.artemis.system;
 
 import com.artemis.BaseSystem;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 
 public class ClearScreenSystem extends BaseSystem {
     
-    @Override
+	private Color clearColor;
+	
+    public ClearScreenSystem() {
+		this(Color.BLACK);
+	}
+
+	public ClearScreenSystem(Color clearColor) {
+		this.clearColor = clearColor;
+	}
+
+	@Override
     protected void processSystem() {
-        Gdx.gl.glClearColor(0.1f, 0.1f, 0.1f, 1);
+        Gdx.gl.glClearColor(clearColor.r, clearColor.g, clearColor.b, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
     }
+
+	public Color getClearColor() {
+		return clearColor;
+	}
+
+	public void setClearColor(Color clearColor) {
+		this.clearColor = clearColor;
+	}
     
 }
